13f339b717cc9a27cd63e3c3c014bd91758b0605
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-08-13 08:19:09 +0530

    2 complete

--- Algo_2.java
+++ Algo_2.java
@@ -2,12 +2,30 @@ public class Algo_2 {
     public static void main(String[] args) {
 
         int[] nums = {-5, 2, 7, 10, 58, -7, 8, 23};
+        int maximumNum = 0;
+        int minimumNum = nums[1];
 
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
-                if(i<j)
+                if(i<j)continue;
+                if(nums[i]<nums[j]){
+                    maximumNum = nums[j];
+                }
             }
         }
+
+        System.out.println("Maximum Number is : " + maximumNum);
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if(i<j)continue;
+                if(minimumNum>nums[j]){
+                    minimumNum = nums[j];
+                }
+            }
+        }
+
+        System.out.println("Minimum Number is : " + minimumNum);
         
     }
     
