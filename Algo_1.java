d466d9558b2598c54600baee2e428dbf4d8ec306
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-08-13 07:49:52 +0530

    1 completed

--- Algo_1.java
+++ Algo_1.java
@@ -8,13 +8,36 @@ public class Algo_1{
             System.out.print("Input a Positive Integer number: ");
             int inputNum = scanner.nextInt();
             scanner.nextLine();
+            String dispay = "Fibonacci sequence upto ";
+            int x = 0;
+            int y = 1;
+            int z = 0;
+            boolean flag = true;
 
             if(!(inputNum>0)){
                 System.out.println("This number is not positive number...");
                 continue;
             }
 
-            
+            System.out.print(dispay + inputNum + " : " );
+            System.out.print(x + " ,");
+            System.out.print(y + " ,");
+
+            do{
+                                
+                z = x + y ;
+                
+                if(z>inputNum){
+                    flag = false;
+                    System.out.println("\b\b  \n");
+                }else{
+                    System.out.print(z + " ,");
+                    x = y;
+                    y = z;
+                }
+
+            }while(flag);
+
 
         }while(true);
 
