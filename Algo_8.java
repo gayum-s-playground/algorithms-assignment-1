385aad0c4dc6aa654e898ea4096c2505c9039e92
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-08-13 14:48:08 +0530

    initial commit

---
+++ Algo_8.java
@@ -0,0 +1,33 @@
+public class Algo_8 {
+    public static void main(String[] args) {
+
+        int[] numA = {5, 7, -2, 3, 4, 6, -8};
+        int[] numB = {7, 8, -8, 2, 1, -9, 6};
+
+        /*------------------------PART01---------------------------- */
+
+        System.out.print("\nnumA \u2229 numB : ");
+        for (int i = 0; i < numA.length; i++) {
+            for (int j = 0; j < numB.length; j++) {
+                                
+                if(numA[i]==numB[j]){
+                    System.out.print(numA[i] + " ,");
+                    continue;
+                }
+            }
+        }
+        System.out.print("\b\b  \n");
+
+        /*------------------------PART02---------------------------- */
+
+        System.out.print("\nnumA \u222A numB : ");
+        for (int i = 0; i < numA.length; i++) {
+            System.out.print(numA[i] + " ,");   
+                  
+        }
+
+       
+
+    }
+    
+}
