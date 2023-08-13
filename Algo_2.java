29aacdddf06fa4560966f53a30c5065492d26f44
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-08-13 07:55:35 +0530

    initial commit

---
+++ Algo_2.java
@@ -0,0 +1,14 @@
+public class Algo_2 {
+    public static void main(String[] args) {
+
+        int[] nums = {-5, 2, 7, 10, 58, -7, 8, 23};
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if(i<j)
+            }
+        }
+        
+    }
+    
+}
