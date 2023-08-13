c7ac3baca6d3c24e6c9144ddb136de1db5467d29
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-08-13 09:22:34 +0530

    3 complete

--- Algo_3.java
+++ Algo_3.java
@@ -1,6 +1,28 @@
 public class Algo_3 {
     public static void main(String[] args) {
         
+        int[] nums = {10, 20, 30, 40, 50};
+
+        int[] copyOfNums = new int[nums.length];
+
+        for (int i = 0; i < copyOfNums.length; i++) {
+            for (int j = nums.length-1; j >= 0; j--) {
+                
+                copyOfNums[copyOfNums.length-(i+1)]=nums[i];
+
+            }
+        }
+
+        nums = copyOfNums;
+
+        System.out.print("\nnums array : {");
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ,");
+        }
+        
+        System.out.print("\b}\n");
+
     }
     
 }
