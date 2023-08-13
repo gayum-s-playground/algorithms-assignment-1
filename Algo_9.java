a027a066c0367c3ae72d358afb88517b2c15f138
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-08-13 17:42:38 +0530

    9 complete

--- Algo_9.java
+++ Algo_9.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Algo_9 {
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+
         loop:
         do{
             System.out.print("\nInput single word : ");
@@ -18,7 +18,23 @@ public class Algo_9 {
                 }
             }
 
+            char[] copyArray = new char[charArray.length];    
             
+            for (int i = 0; i < charArray.length; i++) {
+                copyArray[i] = charArray[charArray.length-(i+1)];
+            }
+
+            boolean flag = true;
+
+            for (int i = 0; i < copyArray.length; i++) {
+                if(charArray[i]!=copyArray[i]) flag = false;
+            }
+
+            if(flag==false){
+                System.out.println("This is not apalindrome");
+            }else{
+                System.out.println("This is palindrome");
+            }
 
         }while(true);
     }
