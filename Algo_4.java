c460d0c4be3b6ffb0c5c429f59c748f61c4a62cf
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-08-13 10:13:53 +0530

    4 complete

--- Algo_4.java
+++ Algo_4.java
@@ -1,7 +1,22 @@
 public class Algo_4 {
     public static void main(String[] args) {
         
+        int[] nums = {10, 20, 30, 40, 50};
+
         
+        for (int i = 0; i < nums.length/2; i++) {
+
+            int duplicateNum = nums[i];
+            nums[i] = nums[nums.length-(i+1)];
+            nums[nums.length-(i+1)] = duplicateNum;
+
+        }
+
+        System.out.print("\nnums array : {");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ,");
+        }
+        System.out.println("\b\b} ");
 
     }
     
